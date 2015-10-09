85037f30d85c1eb8cf9562142570c3ed1aed1119
Carl-Ola Boketoft <calleboketoft@gmail.com>
2015-10-09 13:46:38 +0200

    singleton simply esm

--- client/app/components/serviceInjection/someService.ts
+++ client/app/components/serviceInjection/someService.ts
@@ -1,9 +1,16 @@
 import { Inject } from 'angular2/angular2'
 import { Http } from 'angular2/http'
+import esmSingleton from './esmSingleton'
 
 export class SomeService {
   constructor (@Inject(Http) http:Http) {
-    console.log('SomeService started, why is this not a singleton?')
+    console.log('SomeService started, note that this is not a singleton')
     this.myValue = 'Calle'
+    if (esmSingleton.singletonDataHere) {
+      console.log('esmSingleton has already been initialized: ', esmSingleton.singletonDataHere)
+    } else {
+      console.log('esmSingleton has NOT been initialized, now being inited')
+      esmSingleton.singletonDataHere = 'yep'
+    }
   }
 }
