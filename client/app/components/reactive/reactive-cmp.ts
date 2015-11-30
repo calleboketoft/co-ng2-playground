c8d2e1e6e7518ffd422d6ec9ca0f9a4bf8ba5839
Carl-Ola Boketoft <calleboketoft@gmail.com>
2015-11-30 14:48:10 +0100

    rx not really ready yet

--- client/app/components/reactive/reactive-cmp.ts
+++ client/app/components/reactive/reactive-cmp.ts
@@ -1,13 +1,31 @@
 // @reactivex/rxjs
 
-import { Component } from 'angular2/angular2'
-import Rx from '@reactivex/rxjs'
+import { Component, EventEmitter } from 'angular2/angular2'
+import * as Rx from '@reactivex/rxjs'
 
 @Component({
   selector: 'reactive-cmp',
   templateUrl: 'app/components/reactive/reactive-cmp.html'
 })
 export class ReactiveCmp {
+
+  clickEventEmitter = new EventEmitter();
+  constructor () {
+    // Video 02
+    var clickStream = this.clickEventEmitter
+    // clickStream.subscribe((val) => console.log(val))
+
+    clickStream.forEach((val) => {
+      console.log(val)
+      return val
+    })
+  }
+
+  clickEvent () {
+    this.clickEventEmitter.next('clix')
+  }
+
+  // Video 01
   nonReactive () {
     var source = ['1', '1', 'foo', '2', '3', '5', 'bar', '8', '13']
     var result = source
@@ -21,8 +39,16 @@ export class ReactiveCmp {
     console.log(resultFiltered)
   }
 
+  // Video 01
   reactive () {
-    Rx.Observable.of('hello world')
-      .subscribe((x) => { console.log(x) })
+    var source = Rx.Observable.interval(400).take(9)
+      .map(i => ['1', '1', 'foo', '2', '3', '5', 'bar', '8', '13'][i])
+
+    var result = source
+      .map(x => parseInt(x))
+      .filter(x => !isNaN(x))
+      .reduce((x, y) => x + y)
+
+    result.subscribe(x => console.log(x))
   }
 }
